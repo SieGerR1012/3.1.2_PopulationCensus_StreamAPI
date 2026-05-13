a714a378ef2ce4344cc07cccaa7bda32879b1f3b
DmitryK96 <sieger1012@gmail.com>
2026-05-13 15:34:33 +0700

    Реализация сортировки людей младше 18 лет, призывников - мужчин от 18 и до 27 лет и потенциально работоспособных людей

--- src/Main.java
+++ src/Main.java
@@ -13,4 +13,32 @@ void main() {
                 Education.values()[new Random().nextInt(Education.values().length)])
         );
     }
+
+    // Поиск количества несовершеннолетних людей младше 18 лет
+    long minorsCount = persons.stream()
+            .filter(p -> p.getAge() < 18) // Фильтр менее 18
+            .count();
+    System.out.println("Несовершеннолетних: " + minorsCount);
+
+    // Поиск фамилий призывников - мужчин от 18 и до 27 лет
+    List<String> recruits = persons.stream()
+            .filter(p -> p.getSex() == Sex.MAN) // Фильтр по мужчинам
+            .filter(p -> p.getAge() >= 18 && p.getAge() <= 27) // Фильтр по возрасту от 18 до 27
+            .map(Person::getFamily)
+            .collect(Collectors.toList());
+    System.out.println("Призывников: " + recruits.size());
+
+    // Сортировка по фамилии список потенциально работоспособных людей с высшим образованием от 18 до 60 лет женщин и до 65 лет мужчин
+    List<Person> workers = persons.stream()
+            .filter(p -> p.getEducation() == Education.HIGHER)
+            .filter(p -> {
+                if (p.getSex() == Sex.WOMAN) {
+                    return p.getAge() >= 18 && p.getAge() <= 60;
+                } else {
+                    return p.getAge() >= 18 && p.getAge() <= 65;
+                }
+            })
+            .sorted(Comparator.comparing(Person::getFamily))
+            .collect(Collectors.toList());
+    System.out.println("Работоспособных: " + workers.size());
 }
